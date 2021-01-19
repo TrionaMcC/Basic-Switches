00a9d096926666e0ed0484278673f46dba58e976
Triona McCarthy <77437011+TrionaMcC@users.noreply.github.com>
2021-01-19 16:41:15 +0000

    Unity Upload

---
+++ 2.1-BasicSwitches/Assets/Scripts/BulbController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbController : MonoBehaviour
+{
+    void Start()
+    {
+        public Animator BulbAnimator;
+    }
+
+    public void TurnOn()
+    {
+        
+    }
+
+    public void TurnOff()
+    {
+
+    }
+}
